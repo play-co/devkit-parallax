aeff7ba5377700d4520121aca17e4e385404ce36
Jimmy Griffith <rogueSkib@gmail.com>
2014-11-26 20:42:49 -0800

    first working version confirmed

--- src/Parallax.js
+++ src/Parallax.js
@@ -11,7 +11,7 @@ var rollFloat = function(mn, mx) { return mn + random() * (mx - mn); };
 var rollInt = function(mn, mx) { return floor(mn + random() * (1 + mx - mn)); };
 
 // image object and config data cache
-var imgCache = {};
+var pieceCache = {};
 
 // the Parallax Class
 exports = Class(function() {
@@ -74,6 +74,9 @@ exports = Class(function() {
 		for (var i = 0, len = config.length; i < len; i++) {
 			var layer = layerPool.obtainView({ parent: this.rootView });
 			layer.reset(config[i], i);
+			// populate initial layers to fill the screen
+			this.moveLayerLeft(layer, 0);
+			this.moveLayerUp(layer, 0);
 		}
 	};
 
@@ -84,27 +87,37 @@ exports = Class(function() {
 	 */
 	this.update = function(x, y) {
 		layerPool.forEachActiveView(function(layer, i) {
-			var layerX = layer.style.x = ~~(x * layer.speedRatioHorz);
-			layer.releaseHorz && this.releasePiecesHorz(layer, layerX);
-			layer.spawnHorz && this.spawnPiecesHorz(layer, layerX);
-			var layerY = layer.style.y = ~~(y * layer.speedRatioVert);
-			layer.releaseVert && this.releasePiecesVert(layer, layerY);
-			layer.spawnVert && this.spawnPiecesVert(layer, layerY);
+			var layerX = ~~(x * layer.xMultiplier);
+			var dx = layerX - layer.style.x;
+			if (dx > 0) {
+				this.moveLayerRight(layer, dx);
+			} else if (dx < 0) {
+				this.moveLayerLeft(layer, dx);
+			}
+
+			var layerY = ~~(y * layer.yMultiplier);
+			var dy = layerY - layer.style.y;
+			if (dy > 0) {
+				this.moveLayerDown(layer, dy);
+			} else if (dy < 0) {
+				this.moveLayerUp(layer, dy);
+			}
 		}, this);
 	};
 
 	/**
-	 * releasePiecesHorz: release pieces out of rootView's horizontal bounds
-	 * ~ layer: the layer whose pieces we're checking
-	 * ~ x: the position of the layer relative to rootView
+	 * moveLayerRight: updates layer position, spawns and releases pieces
+	 * ~ layer: the layer to move
+	 * ~ dx: how far to move
 	 */
-	this.releasePiecesHorz = function(layer, x) {
-		var dx = x - layer.x;
+	this.moveLayerRight = function(layer, dx) {
+		var x = layer.style.x += dx;
 		var rvs = this.rootView.style;
-		var pieces = layer.pieces;
-		var finished = false;
 
-		if (dx > 0) {
+		if (layer.xCanRelease) {
+			// release pieces that have been pushed too far right
+			var pieces = layer.pieces;
+			var finished = false;
 			while (pieces.length && !finished) {
 				var piece = pieces[pieces.length - 1];
 				if (piece.style.x >= -x + rvs.width) {
@@ -113,7 +126,38 @@ exports = Class(function() {
 					finished = true;
 				}
 			}
-		} else if (dx < 0) {
+		}
+
+		if (layer.xCanSpawn) {
+			// spawn to the left if necessary
+			var pieceOptions = layer.pieceOptions;
+			while (layer.xSpawnMin > -x) {
+				if (layer.ordered) {
+					var index = layer.pieceIndex++;
+					if (layer.pieceIndex === pieceOptions.length) {
+						layer.pieceIndex = 0;
+					}
+					layer.spawnPieceLeft(pieceOptions[index], piecePool);
+				} else {
+					layer.spawnPieceLeft(choose(pieceOptions), piecePool);
+				}
+			}
+		}
+	};
+
+	/**
+	 * moveLayerLeft: updates layer position, spawns and releases pieces
+	 * ~ layer: the layer to move
+	 * ~ dx: how far to move
+	 */
+	this.moveLayerLeft = function(layer, dx) {
+		var x = layer.style.x += dx;
+		var rvs = this.rootView.style;
+
+		if (layer.xCanRelease) {
+			// release pieces that have been pushed too far left
+			var pieces = layer.pieces;
+			var finished = false;
 			while (pieces.length && !finished) {
 				var piece = pieces[0];
 				if (piece.style.x + piece.style.width <= -x) {
@@ -123,20 +167,37 @@ exports = Class(function() {
 				}
 			}
 		}
+
+		if (layer.xCanSpawn) {
+			// spawn to the left if necessary
+			var pieceOptions = layer.pieceOptions;
+			while (layer.xSpawnMax < -x + rvs.width) {
+				if (layer.ordered) {
+					var index = layer.pieceIndex++;
+					if (layer.pieceIndex === pieceOptions.length) {
+						layer.pieceIndex = 0;
+					}
+					layer.spawnPieceRight(pieceOptions[index], piecePool);
+				} else {
+					layer.spawnPieceRight(choose(pieceOptions), piecePool);
+				}
+			}
+		}
 	};
 
 	/**
-	 * releasePiecesVert: release pieces out of rootView's vertical bounds
-	 * ~ layer: the layer whose pieces we're checking
-	 * ~ y: the position of the layer relative to rootView
+	 * moveLayerDown: updates layer position, spawns and releases pieces
+	 * ~ layer: the layer to move
+	 * ~ dy: how far to move
 	 */
-	this.releasePiecesVert = function(layer, y) {
-		var dy = y - layer.y;
+	this.moveLayerDown = function(layer, dy) {
+		var y = layer.style.y += dy;
 		var rvs = this.rootView.style;
-		var pieces = layer.pieces;
-		var finished = false;
 
-		if (dy > 0) {
+		if (layer.yCanRelease) {
+			// release pieces that have been pushed too far right
+			var pieces = layer.pieces;
+			var finished = false;
 			while (pieces.length && !finished) {
 				var piece = pieces[pieces.length - 1];
 				if (piece.style.y >= -y + rvs.height) {
@@ -145,7 +206,38 @@ exports = Class(function() {
 					finished = true;
 				}
 			}
-		} else if (dy < 0) {
+		}
+
+		if (layer.yCanSpawn) {
+			// spawn to the left if necessary
+			var pieceOptions = layer.pieceOptions;
+			while (layer.ySpawnMin > -y) {
+				if (layer.ordered) {
+					var index = layer.pieceIndex++;
+					if (layer.pieceIndex === pieceOptions.length) {
+						layer.pieceIndex = 0;
+					}
+					layer.spawnPieceUp(pieceOptions[index], piecePool);
+				} else {
+					layer.spawnPieceUp(choose(pieceOptions), piecePool);
+				}
+			}
+		}
+	};
+
+	/**
+	 * moveLayerUp: updates layer position, spawns and releases pieces
+	 * ~ layer: the layer to move
+	 * ~ dy: how far to move
+	 */
+	this.moveLayerUp = function(layer, dy) {
+		var y = layer.style.y += dy;
+		var rvs = this.rootView.style;
+
+		if (layer.yCanRelease) {
+			// release pieces that have been pushed too far left
+			var pieces = layer.pieces;
+			var finished = false;
 			while (pieces.length && !finished) {
 				var piece = pieces[0];
 				if (piece.style.y + piece.style.height <= -y) {
@@ -155,61 +247,85 @@ exports = Class(function() {
 				}
 			}
 		}
-	};
 
-	// TODO: generalize spawn logic
-	this.spawnPieces = function(layer, y) {
-		var pieceOptions = layer.pieceOptions;
-		while (layer.y >= -y - BG_HEIGHT) {
-			if (layer.ordered) {
-				var index = layer.pieceIndex;
-				layer.pieceIndex++;
-				if (layer.pieceIndex >= pieceOptions.length) {
-					layer.pieceIndex = 0;
+		if (layer.yCanSpawn) {
+			// spawn to the left if necessary
+			var pieceOptions = layer.pieceOptions;
+			while (layer.ySpawnMax < -y + rvs.height) {
+				if (layer.ordered) {
+					var index = layer.pieceIndex++;
+					if (layer.pieceIndex === pieceOptions.length) {
+						layer.pieceIndex = 0;
+					}
+					layer.spawnPieceDown(pieceOptions[index], piecePool);
+				} else {
+					layer.spawnPieceDown(choose(pieceOptions), piecePool);
 				}
-				layer.spawnPiece(pieceOptions[index], piecePool);
-			} else {
-				layer.spawnPiece(choose(pieceOptions), piecePool);
 			}
 		}
 	};
 });
 
-// TODO: generalize layer class and bring up-to-date with parallax changes
+// the Parallax Layer Class
 var LayerView = exports.LayerView = Class(View, function() {
 	var sup = View.prototype;
+	var _uid = 0;
 
 	this.init = function(opts) {
 		sup.init.call(this, opts);
 
-		this.x = 0;
-		this.y = 0;
+		// horizontal properties
 		this.xSpawnMin = 0;
 		this.xSpawnMax = 0;
+		this.xMultiplier = 1;
+		this.xCanSpawn = true;
+		this.xCanRelease = true;
+		this.xGapRange = { min: 0, max: 0 };
+		// vertical properties
 		this.ySpawnMin = 0;
 		this.ySpawnMax = 0;
+		this.yMultiplier = 1;
+		this.yCanSpawn = true;
+		this.yCanRelease = true;
+		this.yGapRange = { min: 0, max: 0 };
+		// misc. layer and piece spawning properties
 		this.index = 0;
+		this.preventTearing = false;
 		this.ordered = false;
 		this.pieceIndex = 0;
-		this.pieceOptions = null;
-		this.gapRange = null;
-		this.speedRatio = 1;
+		this.pieceOptions = [];
 		this.pieces = [];
 	};
 
 	this.reset = function(config, index) {
-		this.x = 0;
-		this.y = 0;
 		this.xSpawnMin = 0;
 		this.xSpawnMax = 0;
+		this.xMultiplier = config.xMultiplier || 0;
+		this.xGapRange = config.xGapRange || { min: 0, max: 0 };
+		this.xCanSpawn = config.xCanSpawn !== void 0
+			? config.xCanSpawn
+			: true;
+		this.xCanRelease = config.xCanRelease !== void 0
+			? config.xCanRelease
+			: this.xCanSpawn;
+
 		this.ySpawnMin = 0;
 		this.ySpawnMax = 0;
+		this.yMultiplier = config.yMultiplier || 0;
+		this.yGapRange = config.yGapRange || { min: 0, max: 0 };
+		this.yCanSpawn = config.yCanSpawn !== void 0
+			? config.yCanSpawn
+			: true;
+		this.yCanRelease = config.yCanRelease !== void 0
+			? config.yCanRelease
+			: this.yCanSpawn;
+
 		this.index = index;
+		this.preventTearing = config.preventTearing || false;
 		this.ordered = config.ordered || false;
 		this.pieceIndex = 0;
 		this.pieceOptions = config.pieceOptions;
-		this.gapRange = config.gapRange || null;
-		this.speedRatio = config.speedRatio || 1;
+		this.pieces = [];
 
 		var s = this.style;
 		s.width = config.width || 1;
@@ -220,56 +336,106 @@ var LayerView = exports.LayerView = Class(View, function() {
 		var pieceOptions = config.pieceOptions;
 		for (var i = 0; i < pieceOptions.length; i++) {
 			var piece = pieceOptions[i];
-			!imgCache[piece.id] && this.cacheImage(piece);
+			if (piece.id === void 0) {
+				piece.id = _uid++;
+			}
+			!pieceCache[piece.id] && this.cachePiece(piece);
 		}
 	};
 
-	this.cacheImage = function(data) {
-		var imgData = imgCache[data.id] = {};
-		var img = imgData.image = new Image({ url: data.image });
-		var b = img.getBounds();
-		imgData.x = data.x || 0;
-		imgData.y = data.y || 0;
-		imgData.width = data.width || b.width + b.marginLeft + b.marginRight;
-		imgData.height = data.height || b.height + b.marginTop + b.marginBottom;
-		imgData.anchorX = data.anchorX || imgData.width / 2;
-		imgData.anchorY = data.anchorY || imgData.height / 2;
-		imgData.scale = data.scale !== void 0 ? data.scale : 1;
-		imgData.opacity = data.opacity !== void 0 ? data.opacity : 1;
-		imgData.flipX = data.flipX || false;
-		imgData.compositeOperation = data.compositeOperation || "";
-		imgData.xRange = data.xRange;
+	this.cachePiece = function(data) {
+		var pieceData = pieceCache[data.id] = {};
+		pieceData.img = new Image({ url: data.image });
+		pieceData.x = data.x || 0;
+		pieceData.y = data.y || 0;
+		pieceData.zIndex = data.zIndex || 0;
+		pieceData.r = data.r || 0;
+		var b = pieceData.img.getBounds();
+		pieceData.width = data.width || b.width + b.marginLeft + b.marginRight;
+		pieceData.height = data.height || b.height + b.marginTop + b.marginBottom;
+		pieceData.anchorX = data.anchorX || pieceData.width / 2;
+		pieceData.anchorY = data.anchorY || pieceData.height / 2;
+		pieceData.scale = data.scale !== void 0 ? data.scale : 1;
+		pieceData.scaleX = data.scaleX !== void 0 ? data.scaleX : 1;
+		pieceData.scaleY = data.scaleY !== void 0 ? data.scaleY : 1;
+		pieceData.opacity = data.opacity !== void 0 ? data.opacity : 1;
+		pieceData.flipX = data.flipX || false;
+		pieceData.flipY = data.flipY || false;
+		pieceData.compositeOperation = data.compositeOperation || "";
+		pieceData.styleRanges = data.styleRanges || [];
 	};
 
-	this.spawnPiece = function(data, pool) {
-		var imgData = imgCache[data.id];
-		var piece = pool.obtainView({
-			parent: this,
-			x: this.x + imgData.x,
-			y: this.y + imgData.y,
-			anchorX: imgData.anchorX,
-			anchorY: imgData.anchorX,
-			width: imgData.width,
-			height: imgData.height,
-			scale: imgData.scale,
-			opacity: imgData.opacity
-		});
-		this.pieces.push(piece);
-		// only call setImage if we have to
-		if (piece.currImage !== imgData.image) {
-			piece.currImage = imgData.image;
-			piece.setImage(imgData.image);
-		}
+	this.spawnPieceLeft = function(data, pool) {
+		var piece = this.addPiece(data, pool);
+		var pieceData = pieceCache[data.id];
 		// update misc. style properties
-		piece.style.flipX = imgData.flipX;
-		piece.style.compositeOperation = imgData.compositeOperation;
-		// apply random xRange
-		var xRange = data.xRange;
-		var x = xRange ? rollInt(xRange.min, xRange.max) - imgData.width / 2 : 0;
-		piece.style.x += x;
-		// next spawn with a one pixel overlap to avoid layer tears
-		var gapRange = this.gapRange;
-		var gap = gapRange ? rollInt(gapRange.min, gapRange.max) : 0;
-		this.y -= (imgData.height - 1) + gap;
+		piece.style.x = this.xSpawnMin + pieceData.x - pieceData.width;
+		piece.style.y = this.ySpawnMin + pieceData.y;
+		this.applyStyleRanges(piece, pieceData);
+		// next spawn with a one pixel overlap to avoid layer tears?
+		var tearOffset = this.preventTearing ? 1 : 0;
+		var gap = rollInt(this.xGapRange.min, this.xGapRange.max);
+		this.xSpawnMin = piece.style.x + tearOffset - gap;
+	};
+
+	this.spawnPieceRight = function(data, pool) {
+		var piece = this.addPiece(data, pool);
+		var pieceData = pieceCache[data.id];
+		// update misc. style properties
+		piece.style.x = this.xSpawnMax + pieceData.x;
+		piece.style.y = this.ySpawnMin + pieceData.y;
+		this.applyStyleRanges(piece, pieceData);
+		// next spawn with a one pixel overlap to avoid layer tears?
+		var tearOffset = this.preventTearing ? 1 : 0;
+		var gap = rollInt(this.xGapRange.min, this.xGapRange.max);
+		this.xSpawnMax = piece.style.x + piece.style.width - tearOffset + gap;
+	};
+
+	this.spawnPieceUp = function(data, pool) {
+		var piece = this.addPiece(data, pool);
+		var pieceData = pieceCache[data.id];
+		// update misc. style properties
+		piece.style.x = this.xSpawnMin + pieceData.x;
+		piece.style.y = this.ySpawnMin + pieceData.y - pieceData.height;
+		this.applyStyleRanges(piece, pieceData);
+		// next spawn with a one pixel overlap to avoid layer tears?
+		var tearOffset = this.preventTearing ? 1 : 0;
+		var gap = rollInt(this.yGapRange.min, this.yGapRange.max);
+		this.ySpawnMin = piece.style.y + tearOffset - gap;
+	};
+
+	this.spawnPieceDown = function(data, pool) {
+		var piece = this.addPiece(data, pool);
+		var pieceData = pieceCache[data.id];
+		// update misc. style properties
+		piece.style.x = this.xSpawnMin + pieceData.x;
+		piece.style.y = this.ySpawnMax + pieceData.y;
+		this.applyStyleRanges(piece, pieceData);
+		// next spawn with a one pixel overlap to avoid layer tears?
+		var tearOffset = this.preventTearing ? 1 : 0;
+		var gap = rollInt(this.yGapRange.min, this.yGapRange.max);
+		this.ySpawnMax = piece.style.y + piece.style.height - tearOffset + gap;
+	};
+
+	this.addPiece = function(data, pool) {
+		var piece = pool.obtainView({ parent: this });
+		var pieceData = pieceCache[data.id];
+		piece.updateOpts(pieceData);
+		piece.style.compositeOperation = pieceData.compositeOperation;
+		// only call setImage if we have to
+		if (piece.currImage !== pieceData.img) {
+			piece.currImage = pieceData.img;
+			piece.setImage(pieceData.img);
+		}
+		this.pieces.push(piece);
+		return piece;
+	};
+
+	this.applyStyleRanges = function(piece, pieceData) {
+		var ranges = pieceData.styleRanges;
+		for (var i = 0; i < ranges.length; i++) {
+			var range = ranges[i];
+			piece.style[range[0]] = rollFloat(range[1], range[2]);
+		}
 	};
 });
